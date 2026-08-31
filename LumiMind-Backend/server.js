e78f31df7d30adb583d1dff90372ef02ed811fcc
Pankaj Sobti <pankajsobti10@gmail.com>
2026-08-31 11:43:16 +0530

    Updates 2.0.2

--- LumiMind-Backend/server.js
+++ LumiMind-Backend/server.js
@@ -8,6 +8,9 @@ import { env } from './src/config/env.js';
 import { connectDB } from './src/config/db.js';
 import app from './app.js';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 async function start() {
   await connectDB();
   app.listen(env.PORT, () => {
